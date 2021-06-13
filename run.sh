bc67b7cfc8eaf5ce90e3f186fa6e4690a75f9ea8
LesterHoly <33650692+LesterHoly@users.noreply.github.com>
2021-06-13 10:40:35 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ rm -f README.md
 cd /root/www/myHexoBlog/blog
 echo "run hexo"
 source ~/.bashrc
+npm install -g hexo-cli
 /root/.nvm/versions/node/v16.3.0/bin/hexo clean
 /root/.nvm/versions/node/v16.3.0/bin/hexo g
 /root/.nvm/versions/node/v16.3.0/bin/hexo s

a011f647651f38461f0aa2b374163b4265f4de31
LesterHoly <907282076@qq.com>
2021-06-13 10:36:14 +0800

    add shell script

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+cd ~/myblogs
+git reset --hard master
+git pull
+cd /root/www/myHexoBlog/blog/source/_posts
+rm -f *.md
+cp -r ~/myblogs/*.md .
+rm -f README.md
+cd /root/www/myHexoBlog/blog
 echo "run hexo"
 source ~/.bashrc
 /root/.nvm/versions/node/v16.3.0/bin/hexo clean
